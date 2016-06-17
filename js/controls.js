838d6f7f2ffdd9a0b6a96c39602ed4d4b00f21b8
hulsken <sarahulsken@gmail.com>
2016-06-17 17:15:12 +0200

    17/06/2016 Portfolio + Contact
    Nouveau portfolio
    Début de la partie contact

---
+++ js/controls.js
@@ -0,0 +1,7 @@
+$(function() {
+    //Simple filter controls
+    $('.simplefilter .portfolioli').click(function() {
+        $('.simplefilter .portfolioli').removeClass('active');
+        $(this).addClass('active');
+    });
+});
